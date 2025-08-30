43fb88f57c1b059fbf0f31a7f4e638a5aee41887
Tham <thamdavies@gmail.com>
2025-08-30 14:53:32 +0700

    Add web app manifest and icons for Plei Trust
    - Created a new site.webmanifest file for the Plei Trust web application.
    - Added icons for the app in 192x192 and 512x512 sizes.
    - Set theme and background colors to white and display mode to standalone.

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -14,7 +14,12 @@ class Components::Layout < Components::Base
       head do
         title { @page_info.title }
         meta(name: "viewport", content: "width=device-width, initial-scale=1")
-        stylesheet_link_tag "application", "data-turbo-track": "reload"
+        link(rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96")
+        link(rel: "icon", type: "image/svg+xml", href: "/favicon.svg")
+        link(rel: "shortcut icon", href: "/favicon.ico")
+        link(rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png")
+        link(rel: "manifest", href: "/site.webmanifest")
+        stylesheet_link_tag "application", "data-turbo-track": "reload", preload: true, as: "style"
         javascript_include_tag "application", "data-turbo-track": "reload", type: "module"
       end
 
