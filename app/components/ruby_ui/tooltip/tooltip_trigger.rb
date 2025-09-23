4c2a4bb4363b461bf6b83b454bc3f25622b263b4
Tham <thamdavies@gmail.com>
2025-09-23 21:57:37 +0700

    fix: add space in tooltip trigger data attribute for consistency

--- app/components/ruby_ui/tooltip/tooltip_trigger.rb
+++ app/components/ruby_ui/tooltip/tooltip_trigger.rb
@@ -10,7 +10,7 @@ module RubyUI
 
     def default_attrs
       {
-        data: {ruby_ui__tooltip_target: "trigger"},
+        data: { ruby_ui__tooltip_target: "trigger" },
         variant: :outline,
         class: "peer"
       }
