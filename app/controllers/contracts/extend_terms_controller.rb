35f9f04b7fc61916a3bd7773f3b35dbdf787aa62
Tham <thamdavies@gmail.com>
2025-10-29 19:28:49 +0700

    Remove cancel operation and associated routes for extend terms

--- app/controllers/contracts/extend_terms_controller.rb
+++ app/controllers/contracts/extend_terms_controller.rb
@@ -10,25 +10,9 @@ class Contracts::ExtendTermsController < ApplicationController
     @contract = ctx[:contract].decorate
   end
 
-  def destroy
-    ctx = ExtendTerm::Operations::Cancel.call(params: cancel_params.to_h, current_user:)
-    if ctx.success?
-      flash.now[:notice] = ctx[:message]
-    else
-      err_msg = ctx["contract.default"].errors.messages[:id].first || "Đã có lỗi xảy ra khi hủy gia hạn thêm"
-      flash.now[:alert] = err_msg
-    end
-
-    @contract = ctx[:contract].decorate
-  end
-
   private
 
   def permit_params
     params.require(:form).permit(:contract_id, :number_of_days, :note)
-  end
-
-  def cancel_params
-    params.require(:form).permit(:contract_id).merge(id: params[:id])
   end
 end
