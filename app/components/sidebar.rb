b50cf21da73288c1095fb7bb8eedda0d12b5b2d1
Tham <thamdavies@gmail.com>
2025-12-01 07:44:40 +0700

    fix: update icon for staff management dropdown in sidebar

--- app/components/sidebar.rb
+++ app/components/sidebar.rb
@@ -120,7 +120,7 @@ class Components::Sidebar < Components::Base
                 arial_controls: "dropdown-layouts",
                 data_collapse_toggle: "dropdown-staffs",
                 aria_expanded: view_context.active_paths?([ staffs_path ])) do
-                Remix::Store3Line(class: "flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white")
+                Remix::GroupLine(class: "flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white")
                 span(class: "flex-1 ml-3 text-left whitespace-nowrap", sidebar_toggle_item: "") { "Quản lý nhân viên" }
                 Remix::ArrowDownSLine(class: "w-6 h-6")
               end
