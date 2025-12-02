1fa219043cf5a1e6be721edc587be1034fc16588
Tham <thamdavies@gmail.com>
2025-12-02 21:35:44 +0700

    refactor: encapsulate tenant context in create_capital_customer and create_capital_contract methods

--- app/concepts/branch/operations/create.rb
+++ app/concepts/branch/operations/create.rb
@@ -16,26 +16,29 @@ module Branch::Operations
     }
 
     def create_capital_customer(ctx, model:, current_user:, **)
-      ctx[:seed_capital_customer] = current_user.create_seed_capital_customer(branch: model)
+      ActsAsTenant.with_tenant(model) do
+        ctx[:seed_capital_customer] = current_user.create_seed_capital_customer(branch: model)
+      end
+
       true
     end
 
     def create_capital_contract(ctx, model:, current_user:, **)
-      model.contracts.create!(
-        asset_name: "Vốn khởi tạo",
-        code: SecureRandom.hex(4).upcase,
-        contract_date: Date.current,
-        contract_term: 0,
-        interest_calculation_method: "investment_capital",
-        interest_rate: 0.0,
-        loan_amount: model.invest_amount,
-        interest_period: 0,
-        status: "active",
-        cashier: current_user,
-        contract_type_code: ContractType.codes[:capital],
-        customer: ctx[:seed_capital_customer],
-        created_by: current_user,
-      )
+      ActsAsTenant.with_tenant(model) do
+        model.contracts.create!(
+          contract_date: Date.current,
+          contract_term: 0,
+          interest_calculation_method: InterestCalculationMethod.config[:code][:investment_capital],
+          interest_rate: 0.0,
+          loan_amount: model.invest_amount * 1_000,
+          interest_period: 0,
+          status: "active",
+          cashier: current_user,
+          contract_type_code: ContractType.codes[:capital],
+          customer: ctx[:seed_capital_customer],
+          created_by: current_user,
+        )
+      end
 
       true
     end
