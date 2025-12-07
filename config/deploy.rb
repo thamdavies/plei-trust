26d710fbb4b75c55cebe7d43130d160192af74b8
Tham <thamdavies@gmail.com>
2025-12-07 20:39:02 +0700

    fix: update linked files and SSH keys in deployment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 set :pty, true
 
 # Default value for linked_dirs is []
-append :linked_files, ".env", "config/credentials/production.key"
+append :linked_files, ".env", "config/master.key", "config/credentials/production.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "storage"
 
 # Rails environment
@@ -39,7 +39,7 @@ set :ssh_options, verify_host_key: :secure
 # Global options
 # --------------
 set :ssh_options, {
-  keys: [ "~/.ssh/id_ed25519" ],
+  keys: [ "~/.ssh/myvps" ],
   user: "ubuntu",
   forward_agent: false,
   auth_methods: [ "publickey" ]
